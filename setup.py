efc2e5b912732b7e8dd02a67a8fbcd0ab17ede93
Eugene <me@knst.me>
2022-11-24 17:14:09 +0300

    Added requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='asic-agent',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/bakabtw/ASIC-agent',
+    license='MIT',
+    author='Eugene',
+    author_email='me@knst.me',
+    description='A simple app for managing your ASIC farm based on available grid power'
+)
