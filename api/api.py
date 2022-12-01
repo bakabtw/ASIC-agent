cf05166ee94bd97cc5efd1245bb16fdfe3f8e956
Eugene <me@knst.me>
2022-12-01 10:49:03 +0300

    Added initial support for ASIC monitoring

--- api/api.py
+++ api/api.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from datetime import datetime
+from pony import orm
 
 app = FastAPI()
 
@@ -33,3 +34,52 @@ async def set_power(power: int):
     return {
         'success': True
     }
+
+
+@app.get("/asic_status")
+async def asic_status():
+    # Creating DB
+    db = orm.Database()
+    db.bind(provider='sqlite', filename='asics.db', create_db=True)
+
+    # Defining a table for ASICs
+    class Hosts(db.Entity):
+        id = orm.PrimaryKey(int, auto=True)
+        ip = orm.Required(str)
+        port = orm.Required(int)
+        user = orm.Required(str)
+        password = orm.Required(str)
+        type = orm.Required(str)
+        power = orm.Required(int)
+        phase = orm.Required(str)
+        power_group = orm.Required(int)
+        online = orm.Required(str)
+
+    # Defining a table for power groups
+    class PowerGroups(db.Entity):
+        id = orm.PrimaryKey(int, auto=True)
+        total_power = orm.Required(int)
+        online = orm.Required(str)
+
+    db.generate_mapping()
+
+    with orm.db_session:
+        hosts = Hosts.select()
+        output = []
+
+        for host in hosts:
+            output.append(
+                {
+                    'id': host.id,
+                    'ip': host.ip,
+                    'port': host.port,
+                    'power': host.power,
+                    'phase': host.phase,
+                    'power_group': host.power_group,
+                    'online': host.online
+                }
+            )
+
+    db.disconnect()
+
+    return output
