95cebd6ab0e32cecc9d32ca6dafe24886a88534b
Eugene Konstantinov <git@knst.me>
2023-03-18 00:58:26 +0300

    Updated get_asic_temp()

--- api/api.py
+++ api/api.py
@@ -188,19 +188,12 @@ def get_asic_temp(asic_id: int):
 
         r = api.summary()
     except Exception:
-        pass
-        # return {'detail': 'Error', 'error': 'Cannot connect to the ASIC'}
+        return {'detail': 'Error', 'error': 'Cannot connect to the ASIC'}
 
-    # return r
-    # Temporary dummy response
-    return {
-        'id': asic_id,
-        'temperature': [
-            {'board_id': 0, 'temperature': 81.0},
-            {'board_id': 1, 'temperature': 82.0},
-            {'board_id': 3, 'temperature': 83.0}
-        ]
-    }
+    # Adding ASIC id to response
+    r['id'] = asic_id
+
+    return r
 
 
 @app.get("/asics_temp")
